1b4d99263ae99c1342f32abf42d9317359b314c7
bergabarba <bergabarba@gmail.com>
2019-03-28 20:13:44 -0300

    site products

--- site.php
+++ site.php
@@ -7,9 +7,14 @@ use \Hcode\Model\User;
 
 $app->get('/', function() {
     
+    $products = Product::listAll();
+	
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		"products"=>Product::checkList($products)
+
+	]);
 
 
 });
--- views-cache/categories-menu.06a0132ca595690f464e8dcf71f239f6.rtpl.php
+++ views-cache/categories-menu.06a0132ca595690f464e8dcf71f239f6.rtpl.php
@@ -1,3 +1,2 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android Google</a></li><li><a href="/categories/3">Apple</a></li><li><a href="/categories/7">Lenovo</a></li><li><a href="/categories/4">Motorola</a></li><li><a href="/categories/6">Samsung</a></li>
 
-<li><a href="/categories/1">Android</a></li><li><a href="/categories/2">IOS</a></li><li><a href="/categories/3">Samsung</a></li>
